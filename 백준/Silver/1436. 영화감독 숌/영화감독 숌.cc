b82f8d8abf8054cb9898feaa419781643066a1e0
bokyeommmmm <143463218+bokyeommmmm@users.noreply.github.com>
2026-02-10 16:30:28 +0900

    [Silver V] Title: 영화감독 숌, Time: 56 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;  
+
+    int count = 0; 
+    int num = 666; 
+
+    while (true) {
+        if (to_string(num).find("666") != string::npos) {
+            count++; 
+
+            if (count == N) {
+                cout << num << "\n";
+                break;
+            }
+        }
+
+        num++; 
+    }
+
+    return 0;
+}
