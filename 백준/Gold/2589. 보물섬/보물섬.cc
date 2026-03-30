faa0b6eedac2d6513adf884ad1d437ab4d3d3128
bokyeommmmm <143463218+bokyeommmmm@users.noreply.github.com>
2026-03-30 16:31:16 +0900

    [Gold V] Title: 보물섬, Time: 108 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Gold/2589. 보물섬/보물섬.cc
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int N, M, res;
+int dr[4] = { 1,-1,0,0 };
+int dc[4] = { 0,0,1,-1 };
+vector<vector<int>> board;
+vector<vector<int>> visited;
+queue<pair<int, int>>q;
+
+void bfs()
+{
+	while (!q.empty())
+	{
+		int r = q.front().first;
+		int c = q.front().second;
+		q.pop();
+		for (int i = 0; i < 4; i++)
+		{
+			int nr = r + dr[i];
+			int nc = c + dc[i];
+			if (nr < 0 || nc < 0 || nr >= N || nc >= M)
+				continue;
+			if (board[nr][nc] != 0) //땅아니면패스
+				continue;
+			board[nr][nc] = board[r][c];
+			q.push({ nr,nc });
+		}
+
+	}
+}
+void bfs2()
+{
+	while (!q.empty())
+	{
+		int r = q.front().first;
+		int c = q.front().second;
+		q.pop();
+		for (int i = 0; i < 4; i++)
+		{
+			int nr = r + dr[i];
+			int nc = c + dc[i];
+			if (nr < 0 || nc < 0 || nr >= N || nc >= M)
+				continue;
+			if (visited[nr][nc] !=-1 ) //가봤으면패스
+				continue;
+			if (board[nr][nc] != board[r][c])
+				continue;
+			visited[nr][nc] = visited[r][c] + 1;
+			res = max(res, visited[nr][nc]);
+			q.push({ nr,nc });
+		}
+
+	}
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> N >> M;
+	board.assign(N, vector<int>(M, 0));
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			char c;
+			cin >> c;
+			if (c == 'L')
+				board[i][j] = 0;
+			else
+				board[i][j] = -1;
+
+		}
+	}
+	int idx = 1;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			if (board[i][j] == 1)
+			{
+				q.push({ i,j });
+				board[i][j] = idx;
+				bfs();
+				idx++;
+			}
+		}
+	}
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			if (board[i][j] != -1)
+			{
+				visited.assign(N, vector<int>(M, -1));
+				visited[i][j] = 0;
+				q.push({ i,j });
+				bfs2();
+			}
+		}
+	}
+	cout << res;
+	return 0;
+}
